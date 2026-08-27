f90ab8f9d6c8c1f065da00783a07f6de9615f7c7
Amaan Khan <ampoffice.mumbai2@gmail.com>
2026-08-27 19:27:27 +0530

    added hide section feature

--- app/home/events/page.js
+++ app/home/events/page.js
@@ -1,11 +1,23 @@
 "use client";
 
 import ContentListPage from "@/components/content/ContentListPage";
+import SectionVisibilityPanel from "@/components/content/SectionVisibilityPanel";
 import { crumbsFor, resourceConfigs } from "@/utils/resourceConfigs";
 import { RESOURCES } from "@/utils/constants";
 
 const config = resourceConfigs[RESOURCES.HOME_EVENTS];
 
 export default function HomeEventsPage() {
-  return <ContentListPage {...config} breadcrumbs={crumbsFor(config)} />;
+  return (
+    <ContentListPage
+      {...config}
+      breadcrumbs={crumbsFor(config)}
+      headerExtra={
+        <SectionVisibilityPanel
+          sectionName="home_events"
+          sectionLabel="Home Upcoming Events"
+        />
+      }
+    />
+  );
 }
